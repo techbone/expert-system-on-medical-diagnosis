86e54fc8eb5a35fa79e85d8e7d2f2080aaf1644b
Etudaye <musaawwaletudaye@gmail.com>
2024-08-08 23:23:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class MedicalDiagnosisSystem(KnowledgeEngine):
     @Rule(Diagnosis(name="Malaria"))
     def malaria_treatment(self):
         print("You have been diagnosed with Malaria. Please do seek medical attention immediately.")
-    @Rule(Diagnosis(name="Influenza"))
+    @Rule(Diagnosis(name="Influenza!"))
     def flu_treatment(self):
         print("You have been diagnosed with Influenza. Rest and drink plenty of fluids as advised.")
 
